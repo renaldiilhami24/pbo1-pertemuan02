0af4937c2a8d4ad03c5fd0d93b5d4a8c8942582c
renaldi <renaldiilhami24@gmail.com>
2020-03-23 19:29:05 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ===================");
+  System.out.println("NIDN : 18630574");
+  System.out.println("Nama : Muhammad Renaldi Ilhami"); 
+  System.out.println("==================================");
+ }
+}
---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model; 
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =============");
+  System.out.println("NPM  : 18630574");
+  System.out.println("Nama : Muhammad Renaldi ilhami");
+  System.out.println("===============================");
+ }
+}
